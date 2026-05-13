44228387465d6d931642b01b2bbfd5a8fd2c088b
Rizka Fajar Nugraha <nunutech4.0@gmail.com>
2026-05-13 17:05:51 +0700

    Fix favicon: add svg and png to static_paths

--- personal_brand/lib/personal_brand_web.ex
+++ personal_brand/lib/personal_brand_web.ex
@@ -17,7 +17,7 @@ defmodule PersonalBrandWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images uploads favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images uploads favicon.ico favicon.svg icon-192.png robots.txt)
 
   def router do
     quote do
